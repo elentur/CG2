9d704329ed5cee379deb6b374d21ff3cbedf38aa
Marcus Bätz <s59156@beuth-hochschule.de>
2016-04-18 10:40:02 +0200

    Circle, Point, Star implemented

---
+++ cg-a01.1-canvas/controller/html_controller.js
@@ -0,0 +1,162 @@
+/*
+ * JavaScript / Canvas teaching framwork 
+ * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
+ * changes by Kristian Hildebrand, khildebrand@beuth-hochschule.de
+ * changes by Marcus Bätz
+ *
+ * Module: html_controller
+ *
+ * Defines callback functions for communicating with various 
+ * HTML elements on the page, e.g. buttons and parameter fields.
+ *
+ */
+
+
+/* requireJS module definition */
+define(["jquery", "Line", "Circle","Point","Star"],
+    (function($, Line,Circle,Point,Star) {
+        "use strict";
+
+        /*
+         * define callback functions to react to changes in the HTML page
+         * and provide them with a closure defining context and scene
+         */
+        var HtmlController = function(context,scene,sceneController) {
+
+
+            // generate random X coordinate within the canvas
+            var randomX = function() {
+                return Math.floor(Math.random()*(context.canvas.width-10))+5;
+            };
+
+            // generate random Y coordinate within the canvas
+            var randomY = function() {
+                return Math.floor(Math.random()*(context.canvas.height-10))+5;
+            };
+
+            // generate random radius for circle
+            var randomCircleRadius = function() {
+                return Math.floor(Math.random()*(context.canvas.height)/3);
+            };
+
+            // generate random radius for point
+            var randomPointRadius = function() {
+                return Math.floor(Math.random()*5);
+            };
+            // generate random color in hex notation
+            var randomColor = function() {
+
+                // convert a byte (0...255) to a 2-digit hex string
+                var toHex2 = function(byte) {
+                    var s = byte.toString(16); // convert to hex string
+                    if(s.length == 1) s = "0"+s; // pad with leading 0
+                    return s;
+                };
+
+                var r = Math.floor(Math.random()*25.9)*10;
+                var g = Math.floor(Math.random()*25.9)*10;
+                var b = Math.floor(Math.random()*25.9)*10;
+
+                // convert to hex notation
+                return "#"+toHex2(r)+toHex2(g)+toHex2(b);
+            };
+
+            /*
+             * event handler for "new line button".
+             */
+            $("#btnNewLine").click( (function() {
+
+                // create the actual line and add it to the scene
+                var style = {
+                    width: Math.floor(Math.random()*3)+1,
+                    color: randomColor()
+                };
+
+                var line = new Line( [randomX(),randomY()],
+                    [randomX(),randomY()],
+                    style );
+                scene.addObjects([line]);
+
+                // deselect all objects, then select the newly created object
+                sceneController.deselect();
+                sceneController.select(line); // this will also redraw
+
+            }));
+
+            /*
+             * event handler for "new Circle button".
+             */
+            $("#btnNewCircle").click( (function() {
+
+                // create the actual circle and add it to the scene
+                var style = {
+                    width: Math.floor(Math.random()*3)+1,
+                    color: randomColor()
+                };
+
+                var circle = new Circle( [randomX(),randomY()],
+                    randomCircleRadius(),
+                    style );
+                scene.addObjects([circle]);
+
+                // deselect all objects, then select the newly created object
+                sceneController.deselect();
+                sceneController.select(circle); // this will also redraw
+
+            }));
+
+            /*
+             * event handler for "new point button".
+             */
+            $("#btnNewPoint").click( (function() {
+
+                // create the actual point and add it to the scene
+                var style = {
+                    width: 1,
+                    color: randomColor(),
+                    fill:true
+                };
+
+                var point = new Point( [randomX(),randomY()],
+                    randomPointRadius(),
+                    style );
+                scene.addObjects([point]);
+
+                // deselect all objects, then select the newly created object
+                sceneController.deselect();
+                sceneController.select(point); // this will also redraw
+
+            }));
+            /*
+             * event handler for "new star button".
+             */
+            $("#btnNewStar").click( (function() {
+
+                // create the actual line and add it to the scene
+                var style = {
+                    width: Math.floor(Math.random()*3)+1,
+                    color: randomColor()
+                };
+                var radius = randomCircleRadius();
+                var innerRadius  = Math.random()*(radius-1);
+                var star = new Star( [randomX(),randomY()],
+                    radius,Math.floor(Math.random()*6)+4,innerRadius,
+                    style );
+                scene.addObjects([star]);
+
+                // deselect all objects, then select the newly created object
+                sceneController.deselect();
+                sceneController.select(star); // this will also redraw
+
+            }));
+        };
+
+        // return the constructor function
+        return HtmlController;
+
+
+    })); // require
+
+
+
+            
---
+++ cg-a01.1-canvas/scene/circle.js
@@ -0,0 +1,111 @@
+/*
+ * JavaScript / Canvas teaching framwork 
+ * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
+ * changes by Kristian Hildebrand, khildebrand@beuth-hochschule.de
+ * changes by Marcus Bätz
+ *
+ * Module: circle
+ *
+ * A circle knows how to draw itself into a specified 2D context,
+ * can tell whether a certain mouse position "hits" the object,
+ * and implements the function createDraggers() to create a set of
+ * draggers to manipulate itself.
+ *
+ */
+
+
+/* requireJS module definition */
+define(["util", "vec2", "Scene", "PointDragger"],
+    (function (util, vec2, Scene, PointDragger) {
+
+        "use strict";
+
+        /**
+         *  A circle that can be dragged
+         *  around by its center and is resizeable.
+         *  Parameters:
+         *  - center : array object representing [x,y] coordinates of center of the circle
+         *  - radius : number value representing the radius of the circle
+         *  - lineStyle: object defining width and color attributes for line drawing,
+         *       begin of the form { width: 2, color: "#00FF00" }
+         */
+
+        var Circle = function (center, radius, lineStyle) {
+
+            console.log("creating circle at [" +
+                center[0] + "," + center[1] + "] with radius "+ radius + ".");
+
+            // draw style for drawing the circleLine
+            this.lineStyle = lineStyle || {width: "2", color: "#0000AA"};
+
+            // initial values in case center is undefined
+            this.center = center || [10, 10];
+            // initial values in case radius is undefined
+            this.radius = radius || 10;
+        };
+
+        // draw this circle into the provided 2D rendering context
+        Circle.prototype.draw = function (context) {
+
+            // draw actual circle
+            context.beginPath();
+            context.arc(this.center[0], this.center[1],     // position
+                this.radius,                          // radius
+                0.0, Math.PI * 2,           // start and end angle
+                true);                    // clockwise
+            context.closePath();
+
+            // set drawing style
+            context.lineWidth = this.lineStyle.width;
+            context.strokeStyle = this.lineStyle.color;
+
+            // actually start drawing
+            context.stroke();
+
+        };
+
+        // test whether the mouse position is on this circle segment
+        Circle.prototype.isHit = function (context, pos) {
+
+            // project point on line, get parameter of that projection point
+            var t = vec2.length(vec2.sub(pos,this.center));
+            console.log("t:", t + " " + this.radius );
+            // outside the circle-line segment?
+            return (t <= this.radius +3 && t >= this.radius-3);
+
+        };
+
+        // return list of draggers to manipulate this circle
+        Circle.prototype.createDraggers = function () {
+
+            var draggerStyle = {radius: 4, color: this.lineStyle.color, width: 0, fill: true};
+            var draggers = [];
+
+            // create closure and callbacks for dragger
+            var _circle = this;
+            var getCenter = function () {
+                return _circle.center;
+            };
+            var getRadius = function () {
+                return [_circle.center[0]+_circle.radius,_circle.center[1]];
+            };
+            var setCenter = function (dragEvent) {
+                _circle.center = dragEvent.position;
+            };
+            var setRadius = function (dragEvent) {
+                _circle.radius = dragEvent.position[0]-_circle.center[0]>1?dragEvent.position[0]-_circle.center[0]:1;
+            };
+            draggers.push(new PointDragger(getCenter, setCenter, draggerStyle));
+            draggers.push(new PointDragger(getRadius, setRadius, draggerStyle));
+
+            return draggers;
+
+        };
+
+
+        // this module only exports the constructor for Circle objects
+        return Circle;
+
+    })); // define
+
+    
