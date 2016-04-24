f78f42d333b094007158c8b00ea6632766ea21aa
schubalibre <robertodziuba@googlemail.com>
2016-04-24 19:59:16 +0200

    es fehlte noch eine id am input field

--- cg2-a01.2-kdtree/controller/html_controller.js
+++ cg2-a01.2-kdtree/controller/html_controller.js
@@ -301,11 +301,15 @@ define(["jquery", "Line", "Circle","Point","Star", "KdTree", "util", "kdutil"],
                 };
 
                 var numPoints = parseInt($("#numPoints").attr("value"));
+
+                console.log(numPoints);
+
                 for(var i=0; i<numPoints; ++i) {
                     var point = new Point([randomX(), randomY()], 5,
                         style);
                     scene.addObjects([point]);
                     pointList.push(point);
+
                 }
 
                 // deselect all objects, then select the newly created object
