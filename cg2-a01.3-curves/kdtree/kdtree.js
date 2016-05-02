17c9fc884207c057854a6be923cc4a792c7b2df7
schubalibre <robertodziuba@googlemail.com>
2016-05-02 08:40:20 +0200

    Parametrische Kurve angefangen

---
+++ cg2-a01.3-curves/kdtree/kdtree.js
@@ -0,0 +1,188 @@
+/*
+ * JavaScript / Canvas teaching framwork
+ * (C)opyright Kristian Hildebrand, khildebrand@beuth-hochschule.de
+ *
+ * Module: kdtree
+ *
+ *
+ */
+
+
+/* requireJS module definition */
+define(["kdutil", "vec2", "Scene", "KdNode", "BoundingBox"],
+    (function(KdUtil, vec2, Scene, KdNode, BoundingBox) {
+
+        "use strict";
+
+        /**
+         * Creates a kd-tree. The build function is directly called
+         * on generation
+         *
+         * @param pointList
+         * @constructor
+         */
+        var KdTree = function (pointList) {
+
+            /**
+             *
+             * @param pointList - list of points
+             * @param dim       - current axis
+             * @param parent    - current parent (starts with root)
+             * @param isLeft    - flag if node is left or right child of its parent
+             * @returns returns root node after tree is build
+             */
+            this.build = function(pointList, dim, parent, isLeft) {
+
+                var node = undefined;
+
+                // ===========================================
+                // TODO: implement build tree
+                // ===========================================
+
+                if (pointList.length == 0) return node;
+
+                //<Berechne Split Position in pointlist>
+
+                var median = KdUtil.sortAndMedian(pointList,dim);
+
+                var nextDim = dim == 0 ? 1 : 0;
+
+                // Note: We need to compute the bounding box for EACH new 'node'
+                //       to be able to query correctly
+
+
+                //<Neuen Knoten im Baum erzeugen>
+                node = new KdNode(dim);
+                //<set node.point>
+                node.point = pointList[median];
+
+                // we start with no parent
+
+                var minX = 0,
+                    minY = 0,
+                    maxX = $('#drawing_area').width(),
+                    maxY = $('#drawing_area').height();
+
+                if(parent){
+                    if(isLeft){
+                        if(dim == 0){
+                            minX = parent.bbox.xmin;
+                            minY = parent.bbox.ymin;
+                            maxX = parent.bbox.xmax;
+                            maxY = parent.point.center[1];
+                        }else{
+                            minX = parent.bbox.xmin;
+                            minY = parent.bbox.ymin;
+                            maxX = parent.point.center[0];
+                            maxY = parent.bbox.ymax;
+                        }
+                    }else{
+                        if(dim == 0){
+                            minX = parent.bbox.xmin;
+                            minY = parent.point.center[1];
+                            maxX = parent.bbox.xmax;
+                            maxY = parent.bbox.ymax;
+                        }else{
+                            minX = parent.point.center[0];
+                            minY = parent.bbox.ymin;
+                            maxX = parent.bbox.xmax;
+                            maxY = parent.bbox.ymax;
+                        }
+                    }
+                }
+
+                //<Berechne Bounding Box des Unterbaumes / node.bbox >
+                node.bbox = new BoundingBox(
+                    minX,
+                    minY,
+                    maxX,
+                    maxY,
+                    node.point,
+                    dim);
+
+                //<Extrahiere Punkte für die linke Unterbaumhälfte>
+                node.leftChild = this.build(
+                    pointList.slice(0, median), //<Unterbaum für linke Seite aufbauen>
+                    nextDim,
+                    node,
+                    true);
+                //<Extrahiere Punkte für die rechte Unterbaumhälfte>
+                node.rightChild = this.build(
+                    pointList.slice(median + 1, pointList.length),  //<Unterbaum für rinke Seite aufbauen>
+                    nextDim,
+                    node,
+                    false);
+
+                return node;
+            };
+
+            /**
+             * Given a query point the function return its nearest neighbor by traversing
+             * down the tree
+             *
+             * @param node - current tree node
+             * @param query - query node
+             * @param nearestDistance - current nearest distance to query node
+             * @param currentBest - current best/nearest node to query node
+             * @param dim - current axis (x or y)
+             * @returns closest tree node to query node
+             */
+            this.findNearestNeighbor = function(node, query, currentBest, nearestDistance, dim) {
+
+                if( !node ) {
+                    return currentBest;
+                }
+
+                var closest = currentBest;
+                var closestDistance = nearestDistance;
+
+                var dist = KdUtil.distance(node.point.center, query.center);
+                if( dist < nearestDistance ) {
+                    closestDistance = dist;
+                    closest = node;
+                }
+
+                var a, b;
+                if (dim == 0) {
+                    if ( query.center[0] < node.point.center[0]) {
+                        a = node.leftChild;
+                        b = node.rightChild;
+                    } else {
+                        a = node.rightChild;
+                        b = node.leftChild;
+                    }
+                } else {
+                    if (query.center[1] < node.point.center[1]) {
+                        a = node.leftChild;
+                        b = node.rightChild;
+                    } else {
+                        a = node.rightChild;
+                        b = node.leftChild;
+                    }
+                }
+
+                var nextDim = (dim === 0) ? 1 : 0;
+                if( a && a.bbox.distanceTo(query.center) < closestDistance) {
+                    closest = this.findNearestNeighbor(a, query, closest, closestDistance, nextDim);
+                    closestDistance = KdUtil.distance(closest.point.center, query.center);
+                }
+
+                if( b && b.bbox.distanceTo(query.center) < closestDistance) {
+                    closest = this.findNearestNeighbor(b, query, closest, closestDistance, nextDim);
+                }
+
+                return closest;
+            };
+
+            //
+            this.root = this.build(pointList, 0);
+            console.log(" this is the root: ", this.root);
+
+        };
+
+        return KdTree;
+
+
+    })); // define
+
+
---
+++ cg2-a01.3-curves/scene/point.js
@@ -0,0 +1,105 @@
+/*
+ * JavaScript / Canvas teaching framwork 
+ * (C)opyright Hartmut Schirmacher, hschirmacher.beuth-hochschule.de
+ * changes by Kristian Hildebrand, khildebrand@beuth-hochschule.de
+ * changes by Marcus Bätz
+ *
+ * Module: point
+ *
+ * A point knows how to draw itself into a specified 2D context,
+ * can tell whether a certain mouse position "hits" the object,
+ * and implements the function createDraggers() to create a set of
+ * draggers to manipulate itself.
+ *
+ */
+
+
+/* requireJS module definition */
+define(["util", "vec2", "Scene", "PointDragger"],
+    (function (util, vec2, Scene, PointDragger) {
+
+        "use strict";
+
+        /**
+         *  A point that can be dragged
+         *  around by its center and is resizeable.
+         *  Parameters:
+         *  - center : array object representing [x,y] coordinates of center of the point
+         *  - radius : number value representing the radius of the point
+         *  - lineStyle: object defining width and color attributes for line drawing,
+         */
+
+        var Point = function (center, radius, lineStyle) {
+
+            console.log("creating point at [" +
+                center[0] + "," + center[1] + "] with radius "+ radius + ".");
+
+            // draw style for drawing the pointLine
+            this.lineStyle = lineStyle || {width: "2", color: "#0000AA"};
+
+            // initial values in case center is undefined
+            this.center = center || [10, 10];
+            // initial values in case radius is undefined
+            this.radius = radius || 2;
+        };
+
+        // draw this point into the provided 2D rendering context
+        Point.prototype.draw = function (context) {
+
+            // draw actual point
+            context.beginPath();
+            context.arc(this.center[0], this.center[1],     // position
+                this.radius,                          // radius
+                0.0, Math.PI * 2,           // start and end angle
+                true);                    // clockwise
+            context.closePath();
+
+            // set drawing style
+            context.lineWidth = this.lineStyle.width;
+            context.strokeStyle = this.lineStyle.color;
+            context.fillStyle = this.lineStyle.color;
+
+            // trigger the actual drawing
+            if (this.lineStyle.fill) {
+                context.fill();
+            }
+            // actually start drawing
+            context.stroke();
+
+        };
+
+        // test whether the mouse position is on this point segment
+        Point.prototype.isHit = function (context, pos) {
+
+            // project point on line, get parameter of that projection point
+            var t = vec2.length(vec2.sub(pos,this.center));
+            // outside the point?
+            return (t <= this.radius+ this.lineStyle.width / 2 + 2);
+
+        };
+
+        // return list of draggers to manipulate this point
+        Point.prototype.createDraggers = function () {
+
+            var draggerStyle = {radius: 4, color: this.lineStyle.color, width: 0, fill: true};
+            var draggers = [];
+
+            // create closure and callbacks for dragger
+            var _point = this;
+            var getCenter = function () {
+                return _point.center;
+            };
+            var setCenter = function (dragEvent) {
+                _point.center = dragEvent.position;
+            };
+            draggers.push(new PointDragger(getCenter, setCenter, draggerStyle));
+
+            return draggers;
+        };
+        
+        // this module only exports the constructor for Point objects
+        return Point;
+
+    })); // define
+
+    
