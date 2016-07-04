b4654e4805416c3aa5245dd7a07ff56a8c806485
Marcus Bätz <s59156@beuth-hochschule.de>
2016-07-04 16:28:47 +0200

    Shader repariert

--- cg-a02.1-surfaces/models/planet.js
+++ cg-a02.1-surfaces/models/planet.js
@@ -22,27 +22,23 @@ define(["three", "shaders"],
             // material.uniforms.<uniform-var-name>.value   = <uniform-value>;
             
             var textureLoader = new THREE.TextureLoader();
-            var texture = (function(){
-                return textureLoader.load("textures/explosion.png", function(t){
-                    console.log(t);
-                });
-            })();
 
-            texture;
 
             var material = new THREE.ShaderMaterial({
-                uniforms: THREE.UniformsUtils.merge([
-                    THREE.UniformsLib['lights'],
+                uniforms:
                     {
+                        dayTexture : { type : 't', value :  textureLoader.load("textures/earth_month04.jpg") },
+                        nightTexture : { type : 't', value :  textureLoader.load("textures/earth_at_night_2048.jpg")},
+                        specTexture : { type : 't', value :  textureLoader.load("textures/earth-specular.jpg")},
+                        cloudTexture : { type : 't', value :  textureLoader.load("textures/earth_clouds_2048.jpg")},
+                        lightDirection: {type : 'v3', value: new THREE.Vector3(0.0, 0.0, 1.0)},
                         diffuseColor: {type : 'c', value: new THREE.Color(1.0, 0.0, 0.0)},
                         specColor: {type : 'c', value: new THREE.Color(1.0, 1.0, 1.0)},
-                        exponent: {type : 'f', value: 4.0},
-                        dayTexture : { type : 't', value :  textureLoader.load("textures/explosion.png") }
+                        exponent: {type : 'f', value: 4.0}
                     }
-                ]),
+                ,
                 vertexShader: Shaders.getVertexShader("planet"),
-                fragmentShader: Shaders.getFragmentShader("planet"),
-                lights: true
+                fragmentShader: Shaders.getFragmentShader("planet")
             });
             
             scope.mesh = new THREE.Mesh( new THREE.SphereGeometry(400, 100,100), material );
--- cg-a02.1-surfaces/shaders/planet_fs.glsl
+++ cg-a02.1-surfaces/shaders/planet_fs.glsl
@@ -1,22 +1,16 @@
 precision mediump float;
 
 // uniform lights (we only have the sun)
-
-struct DirectionalLight {
-    vec3 direction;
-    vec3 color;
-    int shadow;
-    float shadowBias;
-    float shadowRadius;
-    vec2 shadowMapSize;
-};
-
-uniform DirectionalLight directionalLights[NUM_DIR_LIGHTS];
-uniform vec3 ambientLightColor[1];
+uniform vec3 lightDirection;
 uniform vec3 diffuseColor;
 uniform vec3 specColor;
 uniform float exponent;
 uniform sampler2D dayTexture;
+uniform sampler2D nightTexture;
+uniform sampler2D cloudTexture;
+uniform sampler2D specTexture;
+
+
 
 // data from the vertex shader
 varying vec4 ecPosition;
@@ -25,25 +19,33 @@ varying vec2 vUv;
 
 
 void main() {
-//    if (NUM_DIR_LIGHTS > 0){
-//
-//        vec3 lambertian = vec3(0.0,0.0,0.0);
-//        float specular = 0.0;
-//
-//        for( int i = 0; i < NUM_DIR_LIGHTS; i ++ ) {
-//
-//            vec3 normal = normalize(ecNormal);
-//            vec3 lightDir = normalize(directionalLights[i].direction * -1.0);
-//            vec3 reflectDir = reflect(-lightDir, normal);
-//            vec3 viewDir = normalize(vec3(-ecPosition));
-//
-//            lambertian = lambertian + max(dot(lightDir,normal), 0.0) * directionalLights[i].color * diffuseColor
-//            + specColor * directionalLights[i].color * pow(max(dot(reflectDir, viewDir), 0.0),exponent);
-//        }
-//
-//        gl_FragColor = vec4(ambientLightColor[0] * diffuseColor + lambertian, 1.0);
-//    }
 
-        gl_FragColor = vec4(texture2D(dayTexture,vUv).rgb,1.0);
+vec3 lightColor = vec3(1.0,1.0,1.0);
+vec3 ambientLight = vec3(0.4,0.45,0.55);
+
+vec3 day = texture2D(dayTexture,vUv).rgb;
+vec3 night = texture2D(nightTexture,vUv).rgb;
+vec3 spec =texture2D(specTexture,vUv).rgb;
+vec3 clouds =texture2D(cloudTexture,vUv).rgb;
+
+
+
+        float specular = 0.0;
+            vec3 normal = normalize(ecNormal);
+            vec3 lightDir = normalize(lightDirection* -1.0);
+            vec3 reflectDir = reflect(-lightDir, normal);
+            vec3 viewDir = normalize(vec3(-ecPosition));
+
+            vec3 earthColor = max(dot(lightDir,normal), 0.0) * lightColor * day +
+            (1.0- max(dot(lightDir,normal), 0.0)) * lightColor * night
+            + spec * lightColor* pow(max(dot(reflectDir, viewDir), 0.0),exponent);
+
+            vec3 cloudColor = max(dot(lightDir,normal), 0.0) * lightColor * clouds +
+           (1.0  - max(dot(lightDir,normal), 0.0)) * ambientLight * clouds;
+
+        gl_FragColor = vec4( earthColor +cloudColor , 1.0);
+
+
+       // gl_FragColor = vec4(texture2D(dayTexture,vUv).rgb,1.0);
 
 }
